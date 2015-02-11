5d7ceee1980ca1f1d56acacc8fc5b9b70692eec4
brandon <josueknd@hotmail.es>
2015-02-11 15:47:39 -0600

    Password checker function edited

--- js/tests.js
+++ js/tests.js
@@ -1,4 +1,4 @@
-describe('PasswordController', function() {
+/*describe('PasswordController', function() {
   beforeEach(module('app'));
 
   var $controller;
@@ -15,6 +15,35 @@ describe('PasswordController', function() {
       $scope.password = 'longerthaneightchars';
       $scope.grade();
       expect($scope.strength).toEqual('strong');
+    });
+  });
+});
+*/
+describe("Pokedex Page testing", function(){
+  describe("Testing security functions",function(){
+    it("should return false if password length is less or equal than 8",function(){
+      expect(checkLength("12345678")).toBe(false);
+    });
+    it("should return true if password length is less or equal than 8",function(){
+      expect(checkLength("123456789")).toBe(true);
+    });
+    it("should return false if password dont have uppercase letters",function(){
+      expect(checkUpper("aaaaaaa")).toBe(false);
+    });
+    it("should return true if password dont have uppercase letters",function(){
+      expect(checkUpper("aaaAaaa")).toBe(true);
+    });
+    it("should return false if password dont have lowercase letters",function(){
+      expect(checkLower("AAAAAAAAA")).toBe(false);
+    });
+    it("should return true if password dont have lowercase letters",function(){
+      expect(checkLower("AAAAAAaAAA")).toBe(true);
+    });
+    it("should return false if password dont have numbers",function(){
+      expect(checkNumber("AAAAAAaAAA")).toBe(false);
+    });
+    it("should return true if password dont have numbers",function(){
+      expect(checkNumber("AAAAA1AaAAA")).toBe(true);
     });
   });
 });
